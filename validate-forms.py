405147fc51504e73bf9564c855af6702698e4e10
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-06-14 14:21:49 -0400

    Created helper for validating all JSON files.

---
+++ validate-forms.py
@@ -0,0 +1,33 @@
+# A helper program to validate all the forms in the output directory.
+
+# Libraries
+import json
+import os
+import glob
+
+# Carry out JSON Schema validation.
+import jsonschema
+
+# Active logging.
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# We read config from `.env`.
+from dotenv import dotenv_values
+config = {
+    **dotenv_values(".env.default"), # default configuration
+    **dotenv_values(".env"),         # override with user-specific® configuration
+    **os.environ,                    # override loaded values with environment variables
+}
+output_dir = config['OUTPUT_DIR']
+
+with open(config['FORM_JSON_SCHEMA']) as f:
+    form_json_schema = json.load(f)
+
+jsonschema.Draft7Validator.check_schema(form_json_schema)
+validator = jsonschema.Draft7Validator(form_json_schema)
+
+for filename in glob.iglob(output_dir + '/**/*.json', recursive=True):
+    with open(filename) as f:
+        data = json.load(f)
+    validator.validate(data)
