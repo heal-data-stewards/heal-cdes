0358081155bf96cd67fdd7f8d31ab7fa36189a60
Gaurav Vaidya <gaurav@renci.org>
2023-09-06 13:39:37 -0400

    Fixed file sorting, added download loop.

--- downloaders/heal_cde_repo_downloader.py
+++ downloaders/heal_cde_repo_downloader.py
@@ -9,6 +9,7 @@
 import collections
 import csv
 import json
+import os
 
 import click
 import logging
@@ -43,51 +44,52 @@ def heal_cde_repo_downloader(output, heal_cde_csv_download):
         lang = 'en'
 
         # At some point in the future, we'll have unique HEAL CDE identifiers that we can use to figure out which of the
-        # files mentioned in heal_cde_csv refer to the same CDE. Until then, we can generate an "id" ourselves based on the
+        # files mentioned in heal_cde_csv refer to the same CDE. Until then, we can generate an "crf_id" ourselves based on the
         # unique stem of filenames.
         if title.endswith('-spanish-crf.docx'):
-            id = title[0:-17]
+            crf_id = title[0:-17]
         elif title.endswith('-crf-pediatric.docx'):
-            id = title[0:-19]
+            crf_id = title[0:-19]
         elif title.endswith('-pediatric-crf.docx'):
-            id = title[0:-19]
+            crf_id = title[0:-19]
         elif title.endswith('-cde-pediatric.xlsx'):
-            id = title[0:-19]
+            crf_id = title[0:-19]
         elif title.endswith('-crf-pediatric-spanish.docx'):
-            id = title[0:-27]
+            crf_id = title[0:-27]
         elif title.endswith('-crf-spanish.docx'):
-            id = title[0:-17]
+            crf_id = title[0:-17]
             lang = 'es'
         elif title.endswith('-crf-spanish.pdf'):
-            id = title[0:-16]
+            crf_id = title[0:-16]
             lang = 'es'
         elif title.endswith('-crf-swedish.docx'):
-            id = title[0:-17]
+            crf_id = title[0:-17]
             lang = 'sv'
         elif title.endswith('-copyright-statement.docx'):
-            id = title[0:-25]
+            crf_id = title[0:-25]
         elif title.endswith('-copyright-statement_.docx'):
-            id = title[0:-26]
+            crf_id = title[0:-26]
         elif title.endswith('-copyright-statment.docx'):
-            id = title[0:-24]
+            crf_id = title[0:-24]
         elif title.endswith('-copyright-statement-pediatric.docx'):
-            id = title[0:-35]
+            crf_id = title[0:-35]
         elif title.endswith('-crf.docx'):
-            id = title[0:-9]
+            crf_id = title[0:-9]
         elif title.endswith('-crf.pdf'):
-            id = title[0:-8]
+            crf_id = title[0:-8]
         elif title.endswith('-cde.pdf'):
-            id = title[0:-8]
+            crf_id = title[0:-8]
         elif title.endswith('-cde.xlsx'):
-            id = title[0:-9]
+            crf_id = title[0:-9]
         elif title.endswith('-cde_.xlsx'):
-            id = title[0:-10]
+            crf_id = title[0:-10]
         elif title.endswith('-cdes.xlsx'):
-            id = title[0:-10]
+            crf_id = title[0:-10]
         elif title.endswith('-crf.xlsx'):
-            id = title[0:-9]
+            crf_id = title[0:-9]
         else:
             raise RuntimeError(f"Could not generate an ID for CRF titled '{title}'.")
+
 
         description = row['Description']
         if row['File Language'] == 'English':
@@ -100,18 +102,48 @@ def heal_cde_repo_downloader(output, heal_cde_csv_download):
             # Hopefully we have a clue in the name.
             pass
 
+        # Get the URL
         url = row['Link to File']
 
-        heal_cde_entries[id].append({
-            'id': id,
+        # The format should still be the last part of the url.
+        url_lc_parts = url.lower().split('.')
+        fmt = url_lc_parts[-1]
+
+        heal_cde_entries[crf_id].append({
+            'crf_id': crf_id,
             'title': title,
             'description': description,
             'lang': lang,
+            'format': fmt,
             'url': url,
             'row': row
         })
 
-    print(json.dumps(heal_cde_entries, indent=2))
+    # Confirm the mapping of input rows to identifiers.
+    logging.debug(json.dumps(heal_cde_entries, indent=2))
+
+    # Set up the output directory.
+    os.makedirs(output)
+
+    # For each identifier, download the XLSX file if that's an option.
+    for crf_id in heal_cde_entries:
+        logging.info(f"Processing {crf_id}")
+        files = heal_cde_entries[crf_id]
+
+        # At the moment we only support a single XLSX file.
+        xlsx_files = list(filter(lambda f: f['format'] == 'xlsx', files))
+        if len(xlsx_files) == 0:
+            logging.warning(f"{crf_id} contains no XLSX files, skipping.")
+            continue
+        elif len(xlsx_files) > 1:
+            raise RuntimeError(
+                f"CRF {crf_id} contains more than one XLSX file, which is not currently supported: {xlsx_files}"
+            )
+
+        xlsx_file = xlsx_files[0]
+        xlsx_file_url = xlsx_file['url']
+
+        logging.info(f"  Downloading XLSX file for {crf_id} from {xlsx_file_url}")
 
 # Run heal_cde_repo_downloader() if not used as a library.
 if __name__ == "__main__":
