f13ab9d0c01395704ef79c4175dba10e8d16ea3e
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-02-21 02:29:25 -0500

    Added support for reading HEAL CDE information.

--- summarizers/finalize-json.py
+++ summarizers/finalize-json.py
@@ -12,7 +12,7 @@
 # so it will probably be hacky and messy, but it would be great to keep an eye on separating things such that
 # how we rewrite this will be obvious in the future.
 #
-
+import csv
 import os
 import json
 import logging
@@ -34,7 +34,12 @@ edges = []
     dir_okay=True,
     allow_dash=False
 ))
-def finalize_json(input_dir):
+@click.option('--heal-crf-mappings', required=False, default=[], multiple=True, type=click.Path(
+    exists=True,
+    file_okay=True,
+    dir_okay=False,
+))
+def finalize_json(input_dir, heal_crf_mappings):
     input_path = click.format_filename(input_dir)
     logging.info(f'Looking for KGX files in {input_path}')
 
@@ -69,8 +74,24 @@ def finalize_json(input_dir):
     crfs = list(filter(lambda n: n['id'].startswith('HEALCDE:'), nodes_by_type['biolink:Publication']))
     logging.info(f'Found {len(crfs)} CRFs.')
 
-    for crf in sorted(crfs, key=lambda crf: crf['name']):
-        print(f"{crf['id']}\t{crf['summary']}")
+    # Add CRF mappings.
+    hdp_id_to_heal_crf_mappings = defaultdict(set)
+    unique_heal_crf_ids = set()
+    for mapping_file in heal_crf_mappings:
+        mapping_path = click.format_filename(mapping_file)
+        logging.info(f'Adding CRF mappings from {mapping_path}')
+        with open(mapping_path, 'r', encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                heal_crf_ids = row['heal_crf_id'].split('|')
+                hdp_ids = row['hdp_id'].split('|')
+
+                for heal_crf_id in heal_crf_ids:
+                    for hdp_id in hdp_ids:
+                        hdp_id_to_heal_crf_mappings[hdp_id].add(heal_crf_id)
+                        unique_heal_crf_ids.add(heal_crf_id)
+
+    logging.info(f"Found mappings from {len(hdp_id_to_heal_crf_mappings)} HDP IDs to {len(unique_heal_crf_ids)} HEAL CRFs.")
 
     # Final step: get rid of nodes that we've pruned out.
     # TODO
